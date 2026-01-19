c63e640c8dd4c3f11eec55fa7b830ed32fdb0e6c
Jack Jansen <Jack.Jansen@cwi.nl>
2026-01-19 12:39:12 +0100

    Make orbbec available to Python.

---
+++ python/_cwipc_orbbec/__init__.py
@@ -0,0 +1,80 @@
+import os
+import ctypes
+import ctypes.util
+import warnings
+from typing import Optional
+from cwipc.util import CwipcError, CWIPC_API_VERSION, cwipc_tiledsource_wrapper
+from cwipc.util import cwipc_tiledsource_p
+from cwipc.util import _cwipc_dll_search_path_collection # type: ignore
+
+__all__ = [
+    "cwipc_orbbec",
+    "cwipc_orbbecplayback",
+    "cwipc_orbbec_dll_load"
+]
+
+_cwipc_orbbec_dll_reference = None
+
+#
+# NOTE: the signatures here must match those in cwipc_util/api.h or all hell will break loose
+#
+def cwipc_orbbec_dll_load(libname : Optional[str]=None) -> ctypes.CDLL:
+    """Load the cwipc_orbbec DLL and assign the signatures (if not already loaded).
+    
+    If you want to load a non-default native library (for example to allow debugging low level code)
+    call this method early, before any other method from this package.
+    """
+    global _cwipc_orbbec_dll_reference
+    if _cwipc_orbbec_dll_reference: return _cwipc_orbbec_dll_reference
+    
+    with _cwipc_dll_search_path_collection(None) as loader:
+        if libname == None:
+            libname = 'cwipc_orbbec'
+        if not os.path.isabs(libname):
+            libname = loader.find_library(libname)
+            if not libname:
+                raise RuntimeError('Dynamic library cwipc_orbbec not found')
+        assert libname
+        _cwipc_orbbec_dll_reference = ctypes.CDLL(libname)
+        if not _cwipc_orbbec_dll_reference:
+            raise RuntimeError(f'Dynamic library {libname} cannot be loaded')
+    
+    _cwipc_orbbec_dll_reference.cwipc_orbbec.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_char_p), ctypes.c_ulong]
+    _cwipc_orbbec_dll_reference.cwipc_orbbec.restype = cwipc_tiledsource_p
+    
+    try:
+        _cwipc_orbbec_dll_reference.cwipc_orbbecplayback.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_char_p), ctypes.c_ulong]
+        _cwipc_orbbec_dll_reference.cwipc_orbbecplayback.restype = cwipc_tiledsource_p
+    except AttributeError:
+        pass
+    return _cwipc_orbbec_dll_reference
+        
+def cwipc_orbbec(conffile : Optional[str]=None) -> cwipc_tiledsource_wrapper:
+    """Returns a cwipc_source object that grabs from a orbbec camera and returns cwipc object on every get() call."""
+    errorString = ctypes.c_char_p()
+    cconffile = None
+    if conffile:
+        cconffile = conffile.encode('utf8')
+    rv = cwipc_orbbec_dll_load().cwipc_orbbec(cconffile, ctypes.byref(errorString), CWIPC_API_VERSION)
+    if errorString and errorString.value and not rv:
+        raise CwipcError(errorString.value.decode('utf8'))
+    if errorString and errorString.value:
+        warnings.warn(errorString.value.decode('utf8'))
+    if rv:
+        return cwipc_tiledsource_wrapper(rv)
+    raise CwipcError("cwipc_orbbec: no cwipc_tiledsource created, but no specific error returned from C library")
+
+def cwipc_orbbecplayback(conffile : Optional[str]=None) -> cwipc_tiledsource_wrapper:
+    """Returns a cwipc_source object that grabs from orbbec camera recordings and returns cwipc objects on every get() call."""
+    errorString = ctypes.c_char_p()
+    cconffile = None
+    if conffile:
+        cconffile = conffile.encode('utf8')
+    rv = cwipc_orbbec_dll_load().cwipc_orbbecplayback(cconffile, ctypes.byref(errorString), CWIPC_API_VERSION)
+    if errorString and errorString.value and not rv:
+        raise CwipcError(errorString.value.decode('utf8'))
+    if errorString and errorString.value:
+        warnings.warn(errorString.value.decode('utf8'))
+    if rv:
+        return cwipc_tiledsource_wrapper(rv)
+    raise CwipcError("cwipc_orbbecplayback: no cwipc_tiledsource created, but no specific error returned from C library")
---
+++ python/test_cwipc_orbbec.py
@@ -0,0 +1,149 @@
+import unittest
+import cwipc
+import _cwipc_orbbec
+import os
+import sys
+import tempfile
+
+if 0:
+    # This code can be used to debug the C++ code in XCode:
+    # - build for XCode with cmake
+    # - build the cwipc_util project
+    # - Fix the pathname for the dylib
+    # - run `python3 test_cwipc_util`
+    # - Attach to python in the XCode debugger
+    # - press return to python3.
+    #
+    # A similar procedure works for debugging with Visual Studio under windows.
+    import _cwipc_orbbec
+    _cwipc_orbbec.cwipc_orbbec_dll_load('C:/Users/VRTogether/VRTogether/cwipc_orbbec/build/bin/RelWithDebInfo/cwipc_orbbec.dll')
+    print('Type return after attaching in XCode debugger (pid=%d) - ' % os.getpid())
+    sys.stdout.flush()
+    sys.stdin.readline()
+
+#
+# Windows search path is horrible. Work around it for testing with an environment variable
+#
+if 'CWIPC_TEST_DLL' in os.environ:
+    filename = os.environ['CWIPC_TEST_DLL']
+    dllobj = _cwipc_orbbec.cwipc_orbbec_dll_load(filename)
+
+#
+# Find directories for test inputs and outputs
+#
+_thisdir=os.path.dirname(os.path.join(os.getcwd(), __file__))
+_topdir=os.path.dirname(_thisdir)
+TEST_FIXTURES_DIR=os.path.join(_topdir, "tests", "fixtures")
+TEST_FIXTURES_PLAYBACK_CONFIG=os.path.join(TEST_FIXTURES_DIR, "input", "recording", "cameraconfig.json")
+TEST_OUTPUT_DIR=os.path.join(TEST_FIXTURES_DIR, "output")
+if not os.access(TEST_OUTPUT_DIR, os.W_OK):
+    TEST_OUTPUT_DIR=tempfile.mkdtemp('cwipc_orbbec_test') # type: ignore
+
+class TestApi(unittest.TestCase):
+    
+    def _open_grabber(self):
+        try:
+            grabber = _cwipc_orbbec.cwipc_orbbec("auto")
+        except cwipc.CwipcError as arg:
+            if str(arg) == 'cwipc_orbbec: no orbbec cameras found':
+                self.skipTest(str(arg))
+            raise
+        return grabber
+        
+    def test_cwipc_orbbec(self):
+        """Test that we can grab a orbbec image"""
+        grabber = None
+        pc = None
+        try:
+            grabber = self._open_grabber()
+            self.assertFalse(grabber.eof())
+            self.assertTrue(grabber.available(True))
+            pc = grabber.get()
+            self.assertIsNotNone(pc)
+            assert pc # Only to keep linters happy
+            # It seems the first pointcloud could be empty. Unsure why...
+            if pc.count() == 0:
+                pc = grabber.get()
+                self.assertIsNotNone(pc)
+                assert pc # Only to keep linters happy
+            self._verify_pointcloud(pc)
+        finally:
+            if grabber: grabber.free()
+            if pc: pc.free()
+
+    def test_cwipc_orbbec_tileinfo(self):
+        """Test that we can get tileinfo from a orbbec grabber"""
+        grabber = None
+        try:
+            grabber = self._open_grabber()
+            nTile = grabber.maxtile()
+            self.assertGreaterEqual(nTile, 1)
+            # Assure the non-tiled-tile exists and points nowhere.
+            tileInfo = grabber.get_tileinfo_dict(0)
+            self.assertIsNotNone(tileInfo)
+            assert tileInfo # Only to keep linters happy
+            self.assertIn('normal', tileInfo)
+            self.assertIn('cameraName', tileInfo)
+            self.assertIn('cameraMask', tileInfo)
+            self.assertIn('ncamera', tileInfo)
+            # Untrue if multiple realsenses connected: self.assertLessEqual(tileInfo['ncamera'], 1)
+            # Test some minimal conditions for other tiles
+            for i in range(1, nTile):
+                tileInfo = grabber.get_tileinfo_dict(i)
+                self.assertIsNotNone(tileInfo)
+                assert tileInfo # Only to keep linters happy
+                if i in (1, 2, 4, 8, 16, 32, 64, 128):
+                    # These tiles should exist and have a normal and camera ID (which may be None)
+                    self.assertIn('normal', tileInfo)
+                    self.assertIn('cameraName', tileInfo)
+        finally:
+            if grabber: grabber.free()
+
+    @unittest.skipIf(sys.platform=='linux' and not 'DISPLAY' in os.environ, "Test requires X server/OpenGL")
+    def test_cwipc_k4aplayback(self):
+        """Test that we can grab a orbbec image from the playback grabber"""
+        grabber = None
+        pc = None
+        try:
+            grabber = _cwipc_orbbec.cwipc_k4aplayback(TEST_FIXTURES_PLAYBACK_CONFIG)
+            self.assertFalse(grabber.eof())
+            self.assertTrue(grabber.available(True))
+            pc = grabber.get()
+            self.assertIsNotNone(pc)
+            assert pc # Only to keep linters happy
+            self._verify_pointcloud(pc)
+        finally:
+            if grabber: grabber.free()
+            if pc: pc.free()
+            
+    @unittest.skipIf(sys.platform=='linux' and not 'DISPLAY' in os.environ, "Test requires X server/OpenGL")
+    def test_cwipc_k4aplayback_seek(self):
+        """Test that we can grab a orbbec image from the playback grabber"""
+        grabber = None
+        pc = None
+        try:
+            grabber = _cwipc_orbbec.cwipc_k4aplayback(TEST_FIXTURES_PLAYBACK_CONFIG)
+            self.assertFalse(grabber.eof())
+            self.assertTrue(grabber.available(True))
+            result = grabber.seek(1600233)
+            self.assertTrue(result)
+            result = grabber.seek(5000000)
+            self.assertFalse(result)
+            pc = grabber.get()
+            self.assertIsNotNone(pc)
+            assert pc # Only to keep linters happy
+            self._verify_pointcloud(pc)
+        finally:
+            if grabber: grabber.free()
+            if pc: pc.free()
+
+    def _verify_pointcloud(self, pc : cwipc.cwipc_wrapper) -> None:
+        points = pc.get_points()
+        self.assertGreater(len(points), 1)
+        halfway = int((len(points)+1)/2)
+        p0 = points[0].x, points[0].y, points[0].z, points[0].r, points[0].g, points[0].b
+        p1 = points[halfway].x, points[halfway].y, points[halfway].z, points[halfway].r, points[halfway].g, points[halfway].b
+        self.assertNotEqual(p0, p1)
+   
+if __name__ == '__main__':
+    unittest.main()
